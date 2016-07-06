aa21de672554ef88c243f68780734ccd19d52a63
Master0r0 <Master0r0@me.com>
2016-07-07 00:00:50 +0100

    Basic Website Welcoming
    Added CSS and a welcome header

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-"Hello World" 
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main.css">
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,800,100,500,300' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div class="header">
+            <h1 class="header">Hello and Welcome!</h1>
+        </div>
+    </body>
+</html>
